2b921031fea4855d9b4780efbdfcbdf515d4f761
Lyso95 <86381287+Lyso95@users.noreply.github.com>
2021-09-01 04:32:39 +0200

    AfterCreating Characters

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using System.Linq;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character { Id = 1, Name = "Sam"}
+        };
+        
+        [HttpGet("GetAll")]
+        
+        public ActionResult<List<Character>> GetGet()
+        {
+            return Ok(characters);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Character> GetSingle(int id)
+        {
+            return Ok(characters.FirstOrDefault(c => c.Id == id));
+        }
+    }
+}
