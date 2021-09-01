72316df6ec6e14285eb72776f0af8abff440c642
Lyso95 <86381287+Lyso95@users.noreply.github.com>
2021-09-01 21:58:22 +0200

    whyunowork

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -19,12 +19,13 @@ namespace dotnet_rpg.Services.CharacterService
         public CharacterService(IMapper mapper)
         {
             _mapper = mapper;
-
         }
         public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            characters.Add(_mapper.Map<Character>(newCharacter));
+            Character character = _mapper.Map<Character>(newCharacter);
+            character.Id = characters.Max(c => c.Id) + 1;
+            characters.Add(character)
             serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
