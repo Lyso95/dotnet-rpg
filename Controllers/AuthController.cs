56bd66c950bfaeb29f15231d7d3ea27ce6f200bb
Lyso95 <86381287+Lyso95@users.noreply.github.com>
2021-09-02 23:27:54 +0200

    AuthLogin

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -30,5 +30,18 @@ namespace dotnet_rpg.Controllers
             return Ok(response);
         }
 
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<string>>> Login(UserLoginDto request)
+        {
+            var response = await _authRepo.Login(
+                request.Username, request.Password
+            );
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
     }
 }
