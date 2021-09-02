5c8b42cf66347392c374f887b0d0e0216be5c53d
Lyso95 <86381287+Lyso95@users.noreply.github.com>
2021-09-02 23:13:02 +0200

    AuthRegister

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,61 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly DataContext _context;
+
+        public AuthRepository(DataContext context)
+        {
+            _context = context;
+
+        }
+
+        public Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public async Task<ServiceResponse<int>> Register(User user, string password)
+        {
+            ServiceResponse<int> response = new ServiceResponse<int>();
+            if(await UserExist(user.Username))
+            {
+                response.Success = false;
+                response.Message = "User already exists.";
+                return response;
+            }
+
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            
+            response.Data = user.Id;
+            return response;
+        }
+
+        public async Task<bool> UserExist(string username)
+        {
+            if(await _context.Users.AnyAsync(x => x.Username.ToLower().Equals(username.ToLower())))
+            {
+                return true;
+            }
+            return false;
+        }
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+    }
+}
