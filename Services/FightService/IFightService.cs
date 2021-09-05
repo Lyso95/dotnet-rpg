5f49b621d99ddf53636fd359c823d873e38606de
Lyso95 <86381287+Lyso95@users.noreply.github.com>
2021-09-05 21:42:54 +0200

    implemented Weaponfights and Skillfights

---
+++ Services/FightService/IFightService.cs
@@ -0,0 +1,14 @@
+using System.Threading.Tasks;
+using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Models;
+
+namespace dotnet_rpg.Services.FightService
+{
+    public interface IFightService
+    {
+        Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request);
+        Task<ServiceResponse<AttackResultDto>> SkillAttack(SkillAttackDto request);
+
+    }
+    
+}
