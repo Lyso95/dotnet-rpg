cf550ff4c2326b6941cf7e5f84705f616eca0b1d
Lyso95 <86381287+Lyso95@users.noreply.github.com>
2021-09-05 15:26:20 +0200

    Debug User

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -92,7 +92,9 @@ namespace dotnet_rpg.Services.CharacterService
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
             try
             {
-                Character character = await _context.Characters.FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
+                Character character = await _context.Characters
+                .Include(c => c.User)
+                .FirstOrDefaultAsync(c => c.Id == updatedCharacter.Id);
                 if(character.User.Id == GetUserId())
                 {
                 
