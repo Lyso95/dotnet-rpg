8b4699be8b2e2efb9051605509023e931818d610
Lyso95 <86381287+Lyso95@users.noreply.github.com>
2021-09-05 19:17:35 +0200

    weapon implementation

--- Dtos/Character/GetCharacterDto.cs
+++ Dtos/Character/GetCharacterDto.cs
@@ -1,3 +1,4 @@
+using dotnet_rpg.Dtos.Weapon;
 using dotnet_rpg.Models;
 
 namespace dotnet_rpg.Dtos.Character
@@ -12,5 +13,6 @@ namespace dotnet_rpg.Dtos.Character
         public int Intelligence { get; set; } = 10;
 
         public RpgClass Class { get; set; } = RpgClass.Knight;
+        public GetWeaponDto Weapon { get; set; }
     }
 }
